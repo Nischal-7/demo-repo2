317c15188e2279852d3f5cb8df56aa83cf051cac
Nischal M R <nischalmadikiri10@gmail.com>
2022-12-09 14:09:44 +0530

    added new line

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This line checks for overriding of a line in two different branches!");
     }
 }
